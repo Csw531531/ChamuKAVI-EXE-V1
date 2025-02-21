611c2f6e4696ba1ae95e0c21ac5f565f84af1131
Csw531531 <senuralakshan456@gmail.com>
2025-02-21 23:41:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=1VQTSAAJ#6j6mOadocmq28Nsu6kNHzDqQnohzSLVfXq-_8vWr4ds",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=tdgVHZoC#OvdN4j71X4BhC3y0Lnl2U33lBa8ZwyyT2T90EDIp0XY"
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
 MODE: process.env.MODE || "public",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
-AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`SEEN YOUR STATUS BY Shenu Whatsapp Bot🗿🍷`",
+AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`SEEN YOUR STATUS BY SHENu Whatsapp Bot🗿🍷🍒`",
 ANTI_CALL: process.env.ANTI_CALL || "false",
 };
